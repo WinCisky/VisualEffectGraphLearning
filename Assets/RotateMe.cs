4c5450be4e8f2e6e5d36607b5e570e9305aed310
Simone S <xsimone97@hotmail.it>
2019-01-26 23:03:04 +0100

    New example (3rd) and update to the old one(2nd)

---
+++ Assets/RotateMe.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMe : MonoBehaviour
+{
+    public float speed = 3f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation *= Quaternion.AngleAxis(Time.deltaTime * speed, Vector3.forward);
+    }
+}
